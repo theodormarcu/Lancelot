12637d76f0d9e435612d0827547418483f199fb8
Theodor Marcu <8714338+theodormarcu@users.noreply.github.com>
2019-04-07 23:32:48 -0400

    Created Dummy Banner. Thanks to Michael S.

--- js/banner.js
+++ js/banner.js
@@ -23,3 +23,52 @@
 'use strict';
 
 /******************************************************************************/
+
+var DEBUG_FLAG = true;
+//text constants
+var BANNER_NORMAL = "This video contains affiliate links. If you click on highlighted links, the creator receives a commission";
+var BANNER_COUPON = "This video may contain affiliate marketing content. The creator may make a commission off of clicks to the highlighted portions of the description";
+var BUTTON_NORMAL = "Exit";
+var BANNER_OPTIONS = {
+	"normal": {
+		"text": BANNER_NORMAL,
+		"button": BUTTON_NORMAL,
+	},
+	"coupon": {
+		"text": BANNER_COUPON,
+		"button": BUTTON_NORMAL,
+	},
+}
+
+
+// Print Only when DEBUG_FLAG is true
+function debug_log(text) {
+    if (DEBUG_FLAG == true) {
+        console.log(text);
+    }
+}
+
+run();
+
+function run() {
+    debug_log("run() Called.");
+    if (!document.getElementById("lancelot-banner")) {
+        // Add observer
+        // addObserver();
+        insertBanner();
+    }
+}
+
+// Insert banner into every HTML page before any other element.
+function insertBanner() {
+    debug_log("insertBanner() called.");
+    if (document.body !== null && !document.getElementById("lancelot-banner")) {
+        var bannerConstants = BANNER_OPTIONS['normal'];
+        var banner = document.createElement("div");
+        banner.innerHTML = "<div id='AdIntuition' style='background-color: red; padding-left:5px; padding-right:10px; padding-bottom:5px; padding-top:1px;'><span style='display:inline-block;'>" + bannerConstants.text + "&nbsp&nbsp</span></div>";
+        banner.id = "lancelot-banner";
+        debug_log(document.body.firstChild);
+        document.body.insertBefore(banner, document.body.firstChild);
+        debug_log("insertBanner() finished running.");
+    }
+}
